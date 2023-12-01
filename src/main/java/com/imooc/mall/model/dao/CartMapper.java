607c15d0831574e3bb53a0817ac3e8ad051edd9a
chenzh12 <chenzh12@midea.com>
2023-12-01 14:59:55 +0800

    imooc_mall 初始化

---
+++ src/main/java/com/imooc/mall/model/dao/CartMapper.java
@@ -0,0 +1,17 @@
+package com.imooc.mall.model.dao;
+
+import com.imooc.mall.model.pojo.Cart;
+
+public interface CartMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Cart record);
+
+    int insertSelective(Cart record);
+
+    Cart selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Cart record);
+
+    int updateByPrimaryKey(Cart record);
+}
---
+++ src/main/java/com/imooc/mall/model/dao/OrderItemMapper.java
@@ -0,0 +1,17 @@
+package com.imooc.mall.model.dao;
+
+import com.imooc.mall.model.pojo.OrderItem;
+
+public interface OrderItemMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(OrderItem record);
+
+    int insertSelective(OrderItem record);
+
+    OrderItem selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(OrderItem record);
+
+    int updateByPrimaryKey(OrderItem record);
+}
